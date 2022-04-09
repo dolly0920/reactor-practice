ce22465a4adf2a6a208c60c9fdb3adaf4bd84f66
dolly0920 <dolly0920@naver.com>
2022-04-09 14:17:20 +0900

    Set up initial commit

---
+++ src/main/java/com/dolly0920/reactorpratice/ReactorPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.dolly0920.reactorpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactorPraticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dolly0920/reactorpratice/ReactorPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolly0920.reactorpratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorPraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
